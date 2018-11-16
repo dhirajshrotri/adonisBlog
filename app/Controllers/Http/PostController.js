436b307a7fe774b69c0b3f7a5dc7e992fb0a7db8
dhirajshrotri <dhiraj.shrotri@gmail.com>
2018-11-16 17:02:25 +0530

    added methods to add new posts

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,17 +1,100 @@
 'use strict'
 
+const Post = use('/App/Models/Post')
+
+const { validate } = use('Validator')
+
 class PostController {
     async index( { view } ){
-        const posts = [
-            {title: 'Post 1', body: 'This is post 1'},
-            {title: 'Post 2', body: 'This is post 2'},
-            {title: 'Post 3', body: 'This is post 3'}
-        ]
+        // const posts = [
+        //     {title: 'Post 1', body: 'This is post 1'},
+        //     {title: 'Post 2', body: 'This is post 2'},
+        //     {title: 'Post 3', body: 'This is post 3'}
+        // ]
+
+        const posts = await Post.all()
+
         return view.render('posts.index', {
             title: 'Latest Posts', 
-            posts: posts
+            posts: posts.toJSON()
         })
     }
+
+    async details({ params, view }){
+        const post = await Post.find(params.id)
+
+        return view.render('posts.details', {
+            post: post
+        })
+    }
+
+    async add({ view }){
+        return view.render('posts.add')
+    }
+
+    async store({ request, response, session }){
+        const validation = await validate(request.all(), {
+            title: 'required|min:3|max:255',
+            body:   'required|min:3'
+        })
+
+        if(validation.fails()){
+            session.withErrors(validation.messages()).flashAll()
+            return response.redirect('back')
+        }
+
+        const post = new Post()
+
+        post.title = request.input('title')
+        post.body = request.input('body')
+
+        await post.save()
+
+        session.flash({ notification: 'Post Added!' })
+        return response.redirect('/posts')
+    }
+
+    async edit({ params, view }){
+        const post = await Post.find(params.id)
+
+        return view.render('post.edit', {
+            post: post
+        })
+    }
+
+    async update({ params, request, response, session }) {
+        // Validate input
+        const validation = await validate(request.all(), {
+          title: 'required|min:3|max:255',
+          body: 'required|min:3'
+        })
+    
+        if(validation.fails()){
+          session.withErrors(validation.messages()).flashAll()
+          return response.redirect('back')
+        }
+    
+        const post = await Post.find(params.id)
+    
+        post.title = request.input('title')
+        post.body = request.input('body')
+    
+        await post.save()
+    
+        session.flash({ notification: 'Post Updated!' })
+    
+        return response.redirect('/posts')
+    }
+    
+    async destroy({ params, session, response }) {
+        const post = await Post.find(params.id)
+    
+        await post.delete()
+    
+        session.flash({ notification: 'Post Deleted!' })
+        
+        return response.redirect('/posts')
+      }
 }
 
 module.exports = PostController
--- start/routes.js
+++ start/routes.js
@@ -14,10 +14,24 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+
 const Route = use('Route')
 
 Route.on('/').render('home')
 
 Route.get('/posts', 'PostController.index')
+
+Route.get('/posts/add', 'PostController.add')
+
+Route.get('/posts/edit/:id', 'PostController.edit')
+
+Route.get('/posts/:id', 'PostController.details')
+
+Route.post('/posts', 'PostController.store')
+
+Route.put('/posts/:id', 'PostController.update')
+
+Route.delete('/posts/:id', 'PostController.destroy')
+
 
 // Route.get('/test', () => 'Hello World!')
